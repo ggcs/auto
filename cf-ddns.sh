c63c0022456c334a70dfa89daa005d140648fdb0
ggcs <41703231+ggcs@users.noreply.github.com>
2020-05-13 02:07:32 +0800

    Update cf-ddns.sh

--- cf-ddns.sh
+++ cf-ddns.sh
@@ -12,13 +12,13 @@ echo "#############################################################"
 
 echo "#############################################################"
 
-stty erase '^H' && read -p "请输入 邮箱 > " ddnsemail
+stty erase '^H' && read -p "请输入 邮箱 > "ddnsemail
 
-stty erase '^H' && read -p "请输入 Key > " auth_key
+stty erase '^H' && read -p "请输入 Key > "auth_key
 
-stty erase '^H' && read -p "请输入 顶级域名> " zone_name
+stty erase '^H' && read -p "请输入 顶级域名> "zone_name
 
-stty erase '^H' && read -p "请输入 二级域名 > " record_name
+stty erase '^H' && read -p "请输入 二级域名 > "record_name
 
 wget --no-check-certificate https://raw.githubusercontent.com/ggcs/cloudflare-api-v4-ddns/master/cf-v4-ddns.sh
 
@@ -33,16 +33,13 @@ sed -i -e 's/www.example.com/$record_name/g' ddns.sh
 sed -i -e 's/example.com/$zone_name/g' ddns.sh
 
 chmod +x ddns.sh
+
 bash ddns.sh
 
-if [ ! -d "//var/spool/cron/crontabs/" ];then
+crontab -l > crontab_test
 
-    echo "*/2 * * * * /root/ddns.sh >/dev/null 2>&1" >> /var/spool/cron/root
-    service cron restart
-else
+echo "*/2 * * * * /root/ddns.sh" >crontab_test
 
-    echo "*/2 * * * * /root/ddns.sh >/dev/null 2>&1" >> /var/spool/cron/crontabs/root
-    service crontab restart
-fi
+crontabcrontab_test
 
 rm -rf cf-ddns.sh
